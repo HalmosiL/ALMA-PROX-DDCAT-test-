195517430e7f509f21385a73a74e324d4fc7f064
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2023-02-20 14:38:16 +0100

    Update test.py

--- alma_prox_segmentation/test.py
+++ alma_prox_segmentation/test.py
@@ -5,15 +5,15 @@ import numpy as np
 import torch
 import cv2
 
-def get_cityscapes_resized(root="", size=None, split="", num_images=1, batch_size=1):
+def get_cityscapes_resized(root="", size=None, split="", num_images=None, batch_size=1):
     val_transform = transform.Compose(
-        [transform.ToTensor()]
+        [transform.ToTensor(),]
     )
 
     image_list_path = root + "/" + split + ".txt"
 
     loader = torch.utils.data.DataLoader(   
-        dataset=SemData(
+        dataset=SemDataSplit(
             split=split,
             data_root=root,
             data_list=image_list_path,
@@ -21,17 +21,19 @@ def get_cityscapes_resized(root="", size=None, split="", num_images=1, batch_siz
             num_of_images=num_images
         ),
         batch_size=1,
-        num_workers=0,
-        pin_memory=True,
-        shuffle = False
+        num_workers=1,
+        pin_memory=True
     )
 
-    return loader
+    with open(image_list_path, 'r') as f:       
+        image_list = np.array([line.split()[0] for line in f]).flatten()
+
+    return loader, label_map_cityscapes, image_list
 
 device = "cuda:2"
 model = load_model("/models/cityscapes/pspnet/ddcat/train_epoch_400.pth", device)
 
-loader = get_cityscapes_resized(
+loader, _, _ = get_cityscapes_resized(
     root="./data/cityscapes/",
     size=None,
     split="val",
