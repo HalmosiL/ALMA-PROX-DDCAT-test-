57ca03cc65030a1f9fdb8ea2bdfe8ec396baefd9
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2023-02-20 14:44:57 +0100

    Update test.py

--- alma_prox_segmentation/test.py
+++ alma_prox_segmentation/test.py
@@ -2,6 +2,7 @@ import transforms as transform
 from dataset import SemData
 from forward import predict
 from model import load_model
+from dataset import SemDataSplit
 
 import numpy as np
 import torch
