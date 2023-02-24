8e8265946c287aa8d202b0cfe312d631665cf91a
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2023-02-24 11:37:41 +0100

    Update model_ingredient.py

--- alma_prox_segmentation/model_ingredient.py
+++ alma_prox_segmentation/model_ingredient.py
@@ -4,8 +4,8 @@ def requires_grad_(model, requires_grad):
     for param in model.parameters():
         param.requires_grad_(requires_grad)
 
-def get_model(device, path):
-    model = load_model(path, device=device)
+def get_model(device, path, mode):
+    model = load_model(path, device, mode)
     model.eval()
     model.to(device)
     requires_grad_(model, False)
